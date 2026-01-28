00422c3fbe7c67f9d728e1b1c1af7e254fdfebd5
overloadd <sergiofernandezair@gmail.com>
2026-01-28 21:31:22 +0100

    Insertada funcion de listar idiomas de audio y subtitulos y teclas toggle para cambiarlos y mostrarlos. Falta testing

--- content.js
+++ content.js
@@ -35,8 +35,12 @@
 	})();
 
 	const getShaka = () => {
-		// Shaka suele estar enterrado en el objeto de la web o el video
+		// Shaka suele estar enterrado en el objeto de la web o el video, se llama al reproductor de yomvi
 		return window.yomvi?.player?.getPlayer?.() || null;
+	};
+
+	const getYomviPlayer = () => {
+		return window.yomvi?.player?.() || null;
 	};
 
 	const getVideo = () => {
@@ -51,7 +55,23 @@
 		if (["INPUT", "TEXTAREA"].includes(document.activeElement?.tagName)) return;
 
 		const video = getVideo();
-		const shaka = getShaka();
+		const shaka = getShaka() || null;
+		const yomvi = getYomviPlayer();
+		let audioTracks = [];
+		let subTracks = [];
+		if(shaka){
+			let audioList = shaka.getAudioLanguages();
+			for (let lang of audioList){
+				if(!lang) break;
+				audioTracks.add(lang);
+			}
+			let subLangs = shaka.getTextLanguages();
+			for (let lang of subLangs){
+				if (!lang) break;
+				subTracks.add(lang);
+			}
+		}
+
 		if (!video) return;
 
 		switch (e.code) {
@@ -81,19 +101,97 @@
 				if (!document.fullscreenElement) video.requestFullscreen();
 				else document.exitFullscreen();
 				break;
+				// Listar idiomas de subtítulos
 			case "KeyC": // Subtítulos (Específico para Shaka Player)
 				if (shaka) {
-					const isVisible = shaka.isTextTrackVisible();
-					shaka.setTextTrackVisibility(!isVisible);
-					showOSD(isVisible ? "Subtítulos: OFF" : "Subtítulos: ON");
+					//let subTracks = yomvi.getTracks("text"); => shaka es mas fiable
+					let langList = "";
+					for (let lang of subTracks){
+						if (!lang) break;
+						langList += `⌨️SUB: ${lang.toUpperCase()}\n`;
+					}
+					langList === "" ? console.error("No hay idiomas para mostrar") : showOSD(langList);
+				} else {
+					console.error("Reproductor no encontrado");
 				}
 				break;
+				// Listar idiomas de audio
+			case "KeyA":
+				if (shaka) {
+					let audioLangList = "";
+					for (let lang of audioTracks){
+						if(!lang) break;
+						audioLangList += `🔊 AUDIO: ${lang.toUpperCase()}`;
+					}
+					audioLangList === "" ? console.error("No hay idiomas para mostrar") : showOSD(audioLangList);
+					break;
+				} else {
+					console.error("Reproductor no encontrado");
+					break;
+				}
+				// Cambiar idioma
+			case "KeyL":
+				e.preventDefault();
+				if (shaka){
+					if (audioTracks.length >= 1){
+						let currIndex = shaka.getVariantTracks().findIndex(track => track.active === true);
+						let audioLang = audioTracks[shaka.getVariantTracks()[currIndex].language];
+						let nextIndex = (currIndex + 1) % audioTracks.length;
+						const nextTrack = audioTracks[nextIndex]; // Siguiente idioma en string
+						console.log(`Cambiando a ${nextTrack}`);
+						showOSD(`🔊${audioLang.toUpperCase()} => 🔊 ${nextTrack.toUpperCase()}`);
+						shaka.selectAudioLanguage(nextTrack, true, 2);
+						break;
+					} else {
+						console.error("No hay idiomas para cambiar");
+						break;
+					}
+				} else {
+					console.error("Reproductor no encontrado");
+					break;
+				}
+				// Cambiar idioma de subtítulos
+			case "KeyS":
+				e.preventDefault();
+				if (shaka){
+					if (subTracks.length >= 1){
+						let currIndex = shaka.getTextTracks().findIndex(track => track.active === true);
+						let subsLang = subTracks[shaka.getTextTracks()[currIndex].language];
+						let nextIndex = (currIndex + 1) % subTracks.length;
+						const nextTrack = subTracks[nextIndex]; // Siguiente idioma en string
+						console.log(`Cambiando a ${nextTrack}`);
+						showOSD(`⌨️SUB: ${subsLang.toUpperCase()} => ⌨️SUB: ${nextTrack.toUpperCase()}`);
+						shaka.selectTextTrack(nextTrack, true, 2);
+						break;
+					} else {
+						console.error("No hay idiomas para cambiar");
+						break;
+					}
+				} else {
+					console.error("Reproductor no encontrado");
+					break;
+				}
+
+			case "KeyD":
+				e.preventDefault();
+				if (shaka) {
+					let visible = false;
+					shaka.setTextTrackVisibility(!visible);
+					visible = !visible;
+					showOSD(`MOSTRAR SUBTITULOS: ${visible === true ? "ON" : "OFF"}`);
+					break;
+				} else {
+					console.error("Reproductor no encontrado");
+					break;
+				}
 			case "KeyM":
 				e.preventDefault();
 				video.muted = !video.muted;
 				// Feedback dinámico basado en el estado real del hardware
 				showOSD(video.muted ? "🔇 SILENCIO" : "🔊 SONIDO ON");
 				break;
+			default:
+				return;
 		}
 	};
 
@@ -101,3 +199,18 @@
 	document.addEventListener("keydown", handleKey, true);
 	console.log("✅ Movistar Enhancer inyectado (Shaka Player Detected)");
 })();
+
+/**
+ * yomvi.player.getPlayer().selectAudioLanguage("qaa", yomvi.player.getTracks('audio'), true);
+ * qaa stands for english?
+ * let subTracks = yomvi.player.getTracks('text');
+ * for (let i = 0 ; i < subTracks.length ; i++){
+ *     console.log(tracks[i].language);
+ * };
+ * let audioTracks = yomvi.player.getTracks('audio');
+ * for (let i = 0 ; i < audioTracks.length ; i++){
+ *     console.log(tracks[i].language);
+ * };
+ * TODO: Find the function to call that returns the audio tracks
+ * TODO: Find the functions to call that returns subtitles
+ */
